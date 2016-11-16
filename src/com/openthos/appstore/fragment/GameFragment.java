06b7001e555d600e51e92ed073a8ec676df62a0e
Cao Yongren <m18410261910@163.com>
2016-11-16 10:42:35 +0000

    OtoAppStore: Supporting show data information about game fragment on application store.
    Signed-off-by: Cao Yongren <m18410261910@163.com>
    Signed-off-by: Chen Gang <chengang@emindsoft.com.cn>

--- src/com/openthos/appstore/fragment/GameFragment.java
+++ src/com/openthos/appstore/fragment/GameFragment.java
@@ -13,11 +13,24 @@ import com.openthos.appstore.R;
 import com.openthos.appstore.app.Constants;
 import com.openthos.appstore.fragment.item.AppLayoutFragment;
 import com.openthos.appstore.fragment.item.AppTypeFragment;
+import com.openthos.appstore.bean.AppLayoutInfo;
+import com.openthos.appstore.bean.GameInfo;
+import com.openthos.appstore.utils.Tools;
+import com.openthos.appstore.utils.NetUtils;
+import android.os.Handler;
+import android.os.Message;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GameFragment extends Fragment {
+
+    public static final int STATE_CODE_SEND_DATA = 0;
+    private List<AppLayoutInfo> mListAppLayoutInfo;
 
     public GameFragment() {
         // Required empty public constructor
@@ -33,12 +46,12 @@ public class GameFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        initData(view);
+        initData();
 
-        initFragment();
+        initFragment(mListAppLayoutInfo);
     }
 
-    private void initFragment() {
+    private void initFragment(List<AppLayoutInfo> listAppLayoutInfo) {
         FragmentManager manager = getActivity().getFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
 
@@ -46,7 +59,7 @@ public class GameFragment extends Fragment {
         appLayoutFragment.setNumColumns(Constants.GRIDVIEW_NUM_COLUMS);
         appLayoutFragment.setFromFragment(Constants.GAME_FRAGMENT);
         appLayoutFragment.setAll(false);
-        appLayoutFragment.setDatas(Constants.getData());
+        appLayoutFragment.setDatas(listAppLayoutInfo);
 
         AppTypeFragment appTypeFragment = new AppTypeFragment();
         appTypeFragment.setFromFragment(Constants.GAME_FRAGMENT);
@@ -58,6 +71,39 @@ public class GameFragment extends Fragment {
         transaction.commit();
     }
 
-    private void initData(View view) {
+    private void initData() {
+        new Thread(new GetData()).start();
+    }
+
+    class GetData implements Runnable {
+        @Override
+        public void run() {
+            try {
+                mListAppLayoutInfo = getListData(NetUtils.getNetStr(Constants.BASEURL + "/list/1"));
+            } catch (JSONException e) {
+                    e.printStackTrace();
+            }
+            mHandler.sendEmptyMessage(STATE_CODE_SEND_DATA);
+        }
+    }
+
+    private Handler mHandler = new Handler(new Handler.Callback(){
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case STATE_CODE_SEND_DATA:
+                    initFragment(mListAppLayoutInfo);
+                    break;
+            }
+            return false;
+        }
+    });
+
+    public List<AppLayoutInfo> getListData(String urlData) throws JSONException {
+        List<AppLayoutInfo> listData = new ArrayList<>();
+        GameInfo gameInfo = new GameInfo(new JSONObject(urlData));
+        AppLayoutInfo appLayoutInfoData = gameInfo.getGameLayoutInfo().getAppLayoutInfo();
+        listData.add(appLayoutInfoData);
+        return listData;
     }
 }
